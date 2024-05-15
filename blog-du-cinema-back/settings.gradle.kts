5e25118a7751508cfc7321f8e12b681a1a425c32
jkjan <ytom2607@gmail.com>
2024-05-15 21:02:43 +0900

    blog-du-cinema 서버 시작

---
+++ blog-du-cinema-back/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "blog-du-cinema-back"
---
+++ blog-du-cinema-back/src/main/kotlin/com/jun/blogducinemaback/BlogDuCinemaBackApplication.kt
@@ -0,0 +1,11 @@
+package com.jun.blogducinemaback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogDuCinemaBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogDuCinemaBackApplication>(*args)
+}
